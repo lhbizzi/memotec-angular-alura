9564bc1b5e61b4e6ee18a0ef2df490b075c68780
Lucas Bizzi <lucas.henrique@orgsystem.com.br>
2026-09-23 14:14:31 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { ListThoughtComponent } from './components/thoughts/list-thought/list-thought.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'list-thought', pathMatch: 'full' },
+  { path: 'create-thought', component: CreateThoughtComponent },
+  { path: 'list-thought', component: ListThoughtComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { ListThoughtComponent } from './components/thoughts/list-thought/list-thought.component';
+import { ThoughtComponent } from './components/thoughts/thought/thought.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateThoughtComponent,
+    ListThoughtComponent,
+    ThoughtComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
